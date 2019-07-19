4d00a5769c0bc97c7b3f355b3c2f3e9e8d477724
GARSON ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGGA5@OOCL.COM>
2019-07-19 17:49:56 +0800

    te

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <input type="text" />
+    
+  </div>
+  
+  
+</template>
+<script>
+
+export default {
+  name: 'app',
+  data: function(){
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    add: function(){
+      this.count++;
+    },
+    extract: function(){
+      this.count--;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
